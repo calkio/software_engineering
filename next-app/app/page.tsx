109ae7c7bc4c54b56b44977acddf358a66d7361e
Влад Давиденко <davidenko.vladislaw2017@yandex.ru>
2024-09-22 13:43:44 +0700

    create header

---
+++ next-app/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import HeaderHome from "@/components/ui/header/header-home";
+
+export default function Home() {
+  return (
+    <>
+      <HeaderHome />
+    </>
+  );
+}
